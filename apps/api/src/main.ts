e88f5e1fd5b2cd324397620a1692a8d1e03011a1
Sedley McCaulsky <sedley_mccaulsky@yahoo.com>
2026-05-08 09:31:31 -0500

    Fix CORS for production

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,29 +6,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
-  const app = await NestFactory.create(AppModule, {
-    logger: ['error', 'warn', 'log', 'debug'],
-  });
-
+  const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT', 3000);
   const prefix = configService.get<string>('API_PREFIX', 'api/v1');
 
   app.setGlobalPrefix(prefix);
-
-  app.enableCors({
-    origin: ['http://localhost:3001', 'http://localhost:19006'],
-    credentials: true,
-  });
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-      transformOptions: { enableImplicitConversion: true },
-    }),
-  );
+  app.enableCors({ origin: '*', credentials: false });
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true, transformOptions: { enableImplicitConversion: true } }));
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('CivicTrace API')
@@ -38,13 +23,10 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('docs', app, document, {
-    swaggerOptions: { persistAuthorization: true },
-  });
+  SwaggerModule.setup('docs', app, document, { swaggerOptions: { persistAuthorization: true } });
 
   await app.listen(port);
   logger.log(`CivicTrace API running on http://localhost:${port}/${prefix}`);
-  logger.log(`Swagger docs at http://localhost:${port}/docs`);
 }
 
 bootstrap();
